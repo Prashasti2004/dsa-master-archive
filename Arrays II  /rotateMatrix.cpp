44bbc69c38159b2d3ad8814472ee8cd14f8e11cc
Prashasti2004 <105813095+Prashasti2004@users.noreply.github.com>
2025-09-30 09:09:18 +0530

    Create rotateMatrix.cpp

---
+++ Arrays II  /rotateMatrix.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Rotate Image
+Platform: Leetcode (Problem #48)
+Approach: Take Transpose of the matrix and then reverse all the rows
+Time Complexity: O(N*N)
+Space Complexity: O(1)
+*/
+
+void rotate(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+}
