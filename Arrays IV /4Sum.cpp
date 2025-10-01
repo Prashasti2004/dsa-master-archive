cece520418264fe26bd98927953ecda381f882de
Prashasti2004 <105813095+Prashasti2004@users.noreply.github.com>
2025-10-01 17:15:12 +0530

    Create 4Sum.cpp

---
+++ Arrays IV /4Sum.cpp
@@ -0,0 +1,61 @@
+/*
+Problem: Given an array nums of n integers, return an array of all the unique quadruplet
+Platform: Leetcode (Problem #18)
+Approach: First sort and then 4 pointer Approach
+Time Complexity: O(n*n*n)
+Space Complexity: O(1)
+*/
+
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<n; i++){
+            //check duplicate for i
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+
+            for(int j=i+1; j<n; j++){
+                //check duplicate for j
+                if(j>i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
+
+                int k = j+1;
+                int l = n-1;
+
+                while(k<l){
+                    long long int sum = nums[i] ;
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+
+                        //check for duplicates in l and k
+                        while(k<l && nums[k]==nums[k-1]){
+                            k++;
+                        }
+
+                        while(k<l && nums[l]==nums[l+1]){
+                            l--;
+                        }
+                    }else if(sum > target){
+                        l--;
+                    }else{
+                        k++;
+                    }
+
+                    
+                }
+            }
+        }
+
+        return ans;
+}
