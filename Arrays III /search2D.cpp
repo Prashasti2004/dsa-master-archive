5cd154d8575499686dc6b6509e9428688646e1da
Prashasti2004 <105813095+Prashasti2004@users.noreply.github.com>
2025-10-01 15:02:01 +0530

    Create search2D.cpp

---
+++ Arrays III /search2D.cpp
@@ -0,0 +1,20 @@
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+          int s = 0;
+          int row = matrix.size();
+          int col = matrix[0].size();
+          int e = (row*col)-1;
+          int mid = s + (e-s)/2;
+
+          while(s<=e){
+            if(matrix[mid/col][mid%col] == target){
+                return true;
+            }else if(matrix[mid/col][mid%col] > target){
+                e = mid-1;
+            }else{
+                s = mid+1;
+            }
+            mid = s + (e-s)/2;
+          }
+
+          return false;
+}
